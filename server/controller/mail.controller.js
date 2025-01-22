fcf9838f5ab08013a499d3d460fa7e2644338deb
Siddharth189 <siddharthiiitg@gmail.com>
2025-01-22 08:43:31 +0530

    removed the secrets

--- server/controller/mail.controller.js
+++ server/controller/mail.controller.js
@@ -1,23 +1,26 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const mailPost = (req, res) => {
   const { name, phone, description } = req.body;
 
   // Create a transporter with your SMTP settings
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMPT_PORT,
     secure: false,
     auth: {
-      user: "siddharthiiitg@gmail.com",
-      pass: "ynta iygy ufbr kzln",
+      user: process.env.SENDER_USERNAME,
+      pass: process.env.PASSCODE,
     },
   });
 
   const mailOptions = {
-    from: "siddharthiiitg@gmail.com",
-    to: "siddharthprg@gmail.com",
-    subject: `Case from website`,
+    from: process.env.SENDER_USERNAME,
+    to: process.env.RECEIVER_USERNAME,
+    subject: process.env.MAIL_SUBJECT,
     text: `
           Name: ${name}
           Phone: ${phone}
